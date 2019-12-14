cbd0dc2d70fe657e0b56d4ba7e6135493b5039e7
wakin- <8th.mail@gmail.com>
2019-12-14 14:41:32 +0900

    branch-channelを実装

--- functions/index.js
+++ functions/index.js
@@ -1,11 +1,21 @@
 const functions = require('firebase-functions')
+const { WebClient } = require('@slack/web-api')
 
-exports.branchChannel = functions.https.onRequest((request, response) => {
-    const channel = ""
-    requestCreateChannel(channel)
-})
+const slack = new WebClient(functions.config().slack.api_key)
 
-function requestCreateChannel(channel) {
-    console.log("request create channel")
-    return null
+function getChannelName(branchName) {
+    return branchName.replace('/', '_')
 }
+
+exports.branchChannel = functions.https.onRequest(async (request, response) => {
+    if (request.headers["x-github-event"] === 'create'
+        && request.body.ref_type === 'branch') {
+        const channelName = getChannelName(request.body.ref)
+        await slack.channels.create({
+            name: channelName
+        })
+        response.status(200).send("create brach: " + channelName)
+    } else {
+        response.status(400).send("not support event")
+    }
+})
